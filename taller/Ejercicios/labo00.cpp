846f9c3498aaaa8bdb66b9548fd107f314cdc97c
mnPanicMeli <manuel.panichelli@mercadolibre.com>
2018-08-26 15:27:09 -0300

    taller: add Ej.2

--- taller/Ejercicios/labo00.cpp
+++ taller/Ejercicios/labo00.cpp
@@ -7,7 +7,15 @@
 int f(int x){
     return x+1;
 }
+
+// Ejercicio 2. Modificar el programa anterior para que f
+//              tome dos parámetros de tipo int y los sume.
+int f2(int x, int y) {
+    return x+y;
+}
+
+
 int main() {
-    std::cout << "El resultado es: " << f(10) << std::endl;
+    std::cout << "El resultado es: " << f2(10,11) << std::endl;
     return 0;
 }
