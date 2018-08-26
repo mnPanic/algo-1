b6adbd5b000b4549ce142fb238daccdb53c7b633
mnPanicMeli <manuel.panichelli@mercadolibre.com>
2018-08-26 15:14:56 -0300

    taller: add initial CLion project

---
+++ taller/Ejercicios/labo00.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Manuel Panichelli on 8/26/18.
+//
+
+#include <iostream>
+
+int f(int x){
+    return x+1;
+}
+int main() {
+    std::cout << "El resultado es: " << f(10) << std::endl;
+    return 0;
+}
