220504f8d4e54e3c95d2725a563f60acb84a5692
mnPanicMeli <manuel.panichelli@mercadolibre.com>
2018-09-09 15:24:02 -0300

    taller: add labo01

---
+++ taller/labo01/labo01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Ingrese un valor entero" << std::endl;
+    int valor = 0;
+    std::cin >> valor;
+    std::cout << "El valor ingresado fue " << valor << std::endl;
+    return 0;
+}
