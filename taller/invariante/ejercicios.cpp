dc2f6b05c1571c8156ab928dcf7fd8100fb03f91
mnPanicMeli <manuel.panichelli@mercadolibre.com>
2018-10-08 19:37:05 -0300

    taller: add Ej. 27

--- taller/invariante/ejercicios.cpp
+++ taller/invariante/ejercicios.cpp
@@ -19,3 +19,26 @@ int sumaTotal(vector<int> s) {
 
     return suma;
 }
+
+
+/* Ejercicio 27. Máximo Común Divisor
+    Encontrar el máximo común divisor entre dos enteros positivos m y n. La post condición del ciclo es
+        Qc ≡ a = b = mcd(a, b).
+
+    I ≡ 0 ≤ a ≤ m ∧ 0 ≤ b ≤ n ∧ mcd(a, b) = mcd(m, n)
+*/
+
+int mcd(int m, int n){
+    int a = m;
+    int b = n;
+    while (a != b) {
+        if (a > b) {
+            a -= b;
+        }
+        else if (b > a) {
+            b -= a;
+        }
+    }
+
+    return a;
+}
--- taller/invariante/main.cpp
+++ taller/invariante/main.cpp
@@ -4,16 +4,14 @@
 using namespace std;
 
 int main() {
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);c
-
-    vector<int> v2 = v;
-    v2.push_back(4);
-
     cout << "Ej. 26: sumaTotal" << endl;
-    cout << "sumaTotal(v) = " << sumaTotal(v) << endl;
-    cout << "sumaTotal(v2) = " << sumaTotal(v2) << endl;
+    cout << "sumaTotal({1, 2, 3}) = " << sumaTotal({1, 2, 3}) << endl;
+    cout << "sumaTotal({1, 2, 3, 4}) = " << sumaTotal({1, 2, 3, 4}) << endl;
+
+    cout << "Ej. 27: mcd" << endl;
+    cout << "mcd(7, 2) = " << mcd(7, 2) << " (debería ser 1)" << endl;
+    cout << "mcd(8, 2) = " << mcd(8, 2) << " (debería ser 2)" << endl;
+    cout << "mcd(2, 8) = " << mcd(2, 8) << " (debería ser 2)" << endl;
+    cout << "mcd(1, 1) = " << mcd(1, 1) << " (debería ser 1)" << endl;
 }
 
