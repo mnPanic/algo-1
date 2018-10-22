edcb546a0a74424fbf75f85251afc3aad57160b3
mnPanicMeli <manuel.panichelli@mercadolibre.com>
2018-10-22 19:12:58 -0300

    taller: ej. 32: adds tests for puntaje
    also modifies CMake

--- taller/tests/test/ej2/puntajeTEST.cpp
+++ taller/tests/test/ej2/puntajeTEST.cpp
@@ -2,3 +2,12 @@
 #include "gtest/gtest.h"
 
 // Escribir tests aca:
+TEST(PuntajeTest, NumeroModTres) {
+    ASSERT_EQ(puntaje(9), 28);
+    ASSERT_EQ(puntaje(12), 22);
+}
+
+TEST(PuntajeTest, NumeroNoModTres) {
+    ASSERT_EQ(puntaje(8), 6);
+    ASSERT_EQ(puntaje(11), 1);
+}
