9fb5bcdc9aabf3b879e0d8513589717a69c4efd9
mnPanicMeli <manuel.panichelli@mercadolibre.com>
2018-11-18 22:32:34 -0300

    agrego ej6

--- ejercicios/main.cpp
+++ ejercicios/main.cpp
@@ -68,12 +68,43 @@ vector<vector<int>> comprimir(vector<int> s) {
     return res;
 }
 
+/******************************** EJ 6 ********************************/
+
+vector<int> meseta(vector<int> s) {
+    vector<int> max = {};
+    vector<int> actual = {s[0]};
+
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i] != s[i - 1]) {
+            if (actual.size() > max.size()) {
+                max = actual;
+            }
+            actual = {};
+        }
+
+        actual.push_back(s[i]);
+    }
+
+    if (actual.size() > max.size()) {
+        max = actual;
+    }
+
+    return max;
+}
+
 int main() {
 
+    cout << "--------------- Ejercicio 5 ---------------" << endl;
     cout << "<1, 2, 3, 4, 5>"       << endl << mToS(comprimir({1,2,3,4,5}))     << endl;
     cout << "<1, 2, 3, 10, 11>"     << endl << mToS(comprimir({1,2,3,10,11}))   << endl;
     cout << "<1, 2, 3, 100, 102>"   << endl << mToS(comprimir({1,2,3,100,102})) << endl;
     cout << "<5, 4, 3, 2, 1>"       << endl << mToS(comprimir({5,4,3,2,1}))     << endl;
+
+
+    cout << "--------------- Ejercicio 6 ---------------" << endl;
+    cout << "<1, 2, 3, 4> -> "          << vToS(meseta({1,2,3,4})) << endl;
+    cout << "<1, 1, 2, 3, 4> -> "       << vToS(meseta({1,1,2,3,4})) << endl;
+    cout << "<1, 1, 2, 2, 3, 3, 3> -> " << vToS(meseta({1, 1, 2, 2, 3, 3, 3})) << endl;
 
 
     return 0;
