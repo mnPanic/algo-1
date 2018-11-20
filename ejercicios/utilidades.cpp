694bfa024b38b7b60851d602e1e22887d08e6785
mnPanicMeli <manuel.panichelli@mercadolibre.com>
2018-11-19 21:30:10 -0300

    actualizo layout de ejercicios

---
+++ ejercicios/utilidades.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "vector"
+#include <string>
+
+
+using namespace std;
+
+/******************************** UTILIDAD ********************************/
+
+string vToS(vector<int> s) {
+    string repr = "{";
+
+    for (int i = 0; i < s.size(); i++) {
+        repr += to_string(s[i]);
+
+        if (i != s.size() - 1) {
+            repr += ", ";
+        }
+    }
+
+    repr += "}";
+
+    return repr;
+}
+
+string sVToS(vector<string> s) {
+    string repr = "{";
+
+    for (int i = 0; i < s.size(); i++) {
+        repr += s[i];
+
+        if (i != s.size() - 1) {
+            repr += ", ";
+        }
+    }
+
+    repr += "}";
+
+    return repr;
+}
+
+string mToS(vector<vector<int>> s) {
+    string repr = "{";
+
+    for (int i = 0; i < s.size(); i++) {
+        if (i != 0) {
+            repr += " ";
+        }
+
+        repr += vToS(s[i]);
+
+        if (i != s.size() - 1) {
+            repr += ",\n";
+        }
+    }
+
+    repr += "}";
+
+    return repr;
+}
